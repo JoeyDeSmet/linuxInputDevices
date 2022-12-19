516917c962bcd8bdc08856ca2761e340377f2707
Joey <joey@de-smet.vlaanderen>
2022-12-19 16:24:18 +0100

    Added alt_key_(up/held) events & direct event input

--- src/include/keyboard.hpp
+++ src/include/keyboard.hpp
@@ -20,10 +20,45 @@ class Keyboard {
     ~Keyboard();
 
   public:
+    /**
+     * @brief 
+     * Directly add an event to handel by creating your own event.
+     * 
+     * @param event 
+     * Event struct, for more information refer to keyboard-events.hpp.
+     * @param callback 
+     * Callback to make when event occurs.
+     */
+    inline void on(KeyboardEvent event, std::function<void()> callback) { 
+      m_event_callback_map.insert_or_assign(event, callback); 
+    }
+
+    /**
+     * @brief
+     * Remove all events associated with the Keycode.
+     * 
+     * @tparam Keycode 
+     * @param keycode 
+     * Keycode to remove events
+     */
     template<typename Keycode, typename std::enable_if<std::is_enum<Keycode>::value>::type* = nullptr>
     void remove_events(Keycode keycode) {
       // Not implemented yet
     }
+
+    /**
+     * @brief 
+     * Remove an event directly.
+     * 
+     * @param event
+     * Event to remove. 
+     */
+    inline void remove_event(KeyboardEvent event) {
+      m_event_callback_map.erase(event);
+    }
+
+  public:
+    //--* The following are methods to make this claass easy to use, they just create the Event struct for you *--//
 
     template<typename Keycode, typename std::enable_if<std::is_enum<Keycode>::value>::type* = nullptr>
     void on_key_up (Keycode keycode, std::function<void ()> callback) {
@@ -69,7 +104,30 @@ class Keyboard {
       m_event_callback_map.insert_or_assign(event, callback);
     }
 
+    template<typename Keycode, typename std::enable_if<std::is_enum<Keycode>::value>::type* = nullptr>
+    void on_alt_key_up(Keycode keycode, Keycode and_keycode, std::function<void()> callback) {
+      KeyboardEvent event = {
+        KeyboardEvents::AltKeyUp,
+        (char) keycode,
+        (char) and_keycode
+      };
+
+      m_event_callback_map.insert_or_assign(event, callback);
+    }
+
+    template<typename Keycode, typename std::enable_if<std::is_enum<Keycode>::value>::type* = nullptr>
+    void on_alt_key_held(Keycode keycode, Keycode and_keycode, std::function<void()> callback) {
+      KeyboardEvent event = {
+        KeyboardEvents::AltKeyHeld,
+        (char) keycode,
+        (char) and_keycode
+      };
+
+      m_event_callback_map.insert_or_assign(event, callback);
+    }
+
   private:
+    // Internal private methods used to handel events.
     void p_event_loop();
     void p_process_key_held(unsigned short keycode);
     void p_process_key_press(unsigned short keycode);
